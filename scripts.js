14d957f822d621cc647150877f46f74d97874bd4
Tucker <49651690+tucktown@users.noreply.github.com>
2023-11-26 13:53:54 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 // Placeholder for future JavaScript code that will handle events, file uploads, and API interactions.
+document.getElementById('csvUpload').addEventListener('change', handleFileSelect, false);
+
+function handleFileSelect(event) {
+    const reader = new FileReader();
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0]);
+}
+
+function handleFileLoad(event) {
+    console.log(event.target.result);
+    // Here you will process the CSV data,
+    // for example, by splitting it into lines and then into values.
+}
+
+document.getElementById('jumpButton').addEventListener('click', function() {
+    const recordId = document.getElementById('recordIdInput').value;
+    // Implement the logic to jump to the specific record
+});
